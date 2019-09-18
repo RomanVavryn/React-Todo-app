78339338d67eda453deee756bf66e37d16139403
Roman Vavtunkevuch <roman.vavrun@gmail.com>
2019-09-18 21:56:12 +0300

    container ended

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './app.css';
 
 export default class App extends Component {
     state = {
@@ -7,7 +8,7 @@ export default class App extends Component {
 
     render() {
         return (
-            <div>
+            <div className='container'>
                 container
             </div>
         )
