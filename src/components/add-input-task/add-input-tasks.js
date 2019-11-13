a284d8cd732363cd113cd7c024fbaceda62dc02b
Roman Vavtunkevuch <roman.vavrun@gmail.com>
2019-11-13 19:07:02 +0200

    add 2 fun to add task to state array

--- src/components/add-input-task/add-input-tasks.js
+++ src/components/add-input-task/add-input-tasks.js
@@ -7,18 +7,29 @@ export default class AddInputTasks extends Component {
     }
 
     hendlChangeTask = (e) => {
-        this.setState({taksText: e.target.value})
+        this.setState({ taksText: e.target.value })
+    }
+
+    addHendl = (event) => {
+        if (event.key === 'Enter') {
+            this.props.addNewTask(this.state.taksText)
+            this.setState({taksText: ''})
+        }
+    }
+
+    textToNull = () => {
+        this.setState({taksText: ''})
     }
 
 
     render() {
         const { taksText } = this.state;
         const { addNewTask } = this.props;
-        
+
         return (
             <div className='input-container'>
-                <input value={taksText} onChange={this.hendlChangeTask} className='input-task' />
-                <button className='addBtn' onClick={() => addNewTask(taksText)}> Add </button>
+                <input value={taksText} onChange={this.hendlChangeTask} onKeyPress={this.addHendl} className='input-task' />
+                <button className='addBtn' onClick={() => {addNewTask(taksText); this.textToNull();}}> Add </button>
             </div>
         )
     }
