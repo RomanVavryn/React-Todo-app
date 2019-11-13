fa87c7bfe086f21209d234a3a922d779ba89a19a
Roman Vavtunkevuch <roman.vavrun@gmail.com>
2019-11-13 20:57:56 +0200

    add todo-list component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,20 +2,27 @@ import React, { Component } from 'react';
 import './app.css';
 
 import AddInputTasks from '../add-input-task';
+import TodoList from '../todo-list';
 
 export default class App extends Component {
     state = {
         todos: [],
     }
 
-    addNewTask = (obj) => {
-        this.setState({todos: [...this.state.todos, obj]})
+    addNewTask = (text) => {
+        let task = {
+            task: text,
+            done: false,
+            import: false
+        }
+        this.setState({todos: [...this.state.todos, task]})
     }
 
     render() {
         return (
             <div className='container'>
                 <AddInputTasks addNewTask={this.addNewTask} />
+                <TodoList todos={this.state.todos} />
             </div>
         )
     }
---
+++ src/components/todo-list/todo-list.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function TodoList (props) {
+    console.log(props)
+    return (
+        <div>hellow </div>
+    )
+}
+
+export default TodoList;
