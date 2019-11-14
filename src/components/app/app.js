48aff54af84e0b0b4acc2e4f6d94498e298ecb6a
Roman Vavtunkevuch <roman.vavrun@gmail.com>
2019-11-14 10:05:57 +0200

    add fn to change task status done

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,20 +9,29 @@ export default class App extends Component {
         todos: [],
     }
 
+    id = 0;
     addNewTask = (text) => {
+
         let task = {
             task: text,
             done: false,
-            import: false
+            import: false,
+            id: this.id,
         }
-        this.setState({todos: [...this.state.todos, task]})
+        this.setState({ todos: [...this.state.todos, task] })
+        this.id++;
+    }
+
+    TaskDone = (id) => {
+        this.state.todos[id].done = !this.state.todos[id].done;
+        this.setState({ todos: this.state.todos })
     }
 
     render() {
         return (
             <div className='container'>
                 <AddInputTasks addNewTask={this.addNewTask} />
-                <TodoList todos={this.state.todos} />
+                <TodoList todos={this.state.todos} TaskDone={this.TaskDone} />
             </div>
         )
     }
